670b196a1a587d3f21dba940c40de6b3bc3b4d5d
iwasa.takahito <iwasa.takahito@HL00780.local>
2020-12-12 01:47:36 +0900

    Initial commit

---
+++ src/IwasaCdkDotnet/Program.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.CDK;
+
+namespace IwasaCdkDotnet
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new IwasaCdkDotnetStack(app, "IwasaCdkDotnetStack");
+
+            app.Synth();
+        }
+    }
+}
