a80c7f602e452cc8dd3c432bf470d7079bb64e14
iwasa.takahito <iwasa.takahito@HL00780.local>
2020-12-12 02:14:03 +0900

    Please enter the commit message for your changes. Lines starting

--- src/IwasaCdkDotnet/IwasaCdkDotnetStack.cs
+++ src/IwasaCdkDotnet/IwasaCdkDotnetStack.cs
@@ -7,17 +7,17 @@ namespace IwasaCdkDotnet
 {
     public class IwasaCdkDotnetStack : Stack
     {
-        internal IwasaCdkDotnetStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
-        {
-             // The CDK includes built-in constructs for most resource types, such as Queues and Topics.
-            var queue = new Queue(this, "IwasaCdkDotnetQueue", new QueueProps
-            {
-                VisibilityTimeout = Duration.Seconds(300)
-            });
+internal IwasaCdkDotnetStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+{
+        // The CDK includes built-in constructs for most resource types, such as Queues and Topics.
+    var queue = new Queue(this, "IwasaCdkDotnetQueue", new QueueProps
+    {
+        VisibilityTimeout = Duration.Seconds(300) 
+    });
 
-            var topic = new Topic(this, "IwasaCdkDotnetTopic");
+    var topic = new Topic(this, "IwasaCdkDotnetTopic");
 
-            topic.AddSubscription(new SqsSubscription(queue));
-        }
+    topic.AddSubscription(new SqsSubscription(queue));
+}
     }
 }
